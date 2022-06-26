d419cd7da7a79f79dc6b1a37bf34a1953f54aed0
Mezhuiev2002 <54850562+Mezhuiev2002@users.noreply.github.com>
2022-06-26 16:13:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf("5409192900:AAFYd6IvCgBA5o4n6VlRlYoVX7XFUY-yFKw")
 bot.start((ctx) => ctx.reply('Hello, user'))
 bot.hears(`hello`, (ctx) => {
     ctx.reply(`${range}`)
-    range = range - 1
+    range = range - 3
 })
 bot.launch()
 

af810d770c97796f38c0997feefd6ae35269bb09
Mezhuiev2002 <54850562+Mezhuiev2002@users.noreply.github.com>
2022-06-28 14:29:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf("5409192900:AAFYd6IvCgBA5o4n6VlRlYoVX7XFUY-yFKw")
 bot.start((ctx) => ctx.reply('Hello, user'))
-bot.hears(`hello`, (ctx) => ctx.reply('Привіт!!!'))
+bot.hears(`hello`, (ctx) => ctx.reply('Привіт'))
 bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))

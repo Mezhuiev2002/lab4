0534345f7e10a7de5f2476aac8e8e76554651327
Mezhuiev2002 <54850562+Mezhuiev2002@users.noreply.github.com>
2022-06-28 14:03:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const { Telegraf } = require('telegraf')
 
-let range = 312
 const bot = new Telegraf("5409192900:AAFYd6IvCgBA5o4n6VlRlYoVX7XFUY-yFKw")
 bot.start((ctx) => ctx.reply('Hello, user'))
-bot.hears(`hello`, (ctx) => {
-    ctx.reply(`${range}`)
-    range = range - 4
-})
+bot.hears(`hello`, (ctx) => ctx.reply('Привіт!!!'))
 bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
